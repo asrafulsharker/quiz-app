276ae137e9a86d83c58e4cec97f482828ca6861c
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2020-12-14 19:51:07 +0600

    compleate full package

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import "./assets/style.css";
+import quizService from "./quizService";
+import QuestionBox from './components/QuestionBox';
+import Result from './components/Result';
+class QuizApp extends Component {
+    state = {
+        questionBank: [],
+        score:0,
+        response: 0,
+    };
+    getQuestions=()=>{
+        quizService().then(question=>{
+            this.setState({
+                questionBank: question
+            });
+        });
+    };
+    computeAnswer =(answer,correctAnswer)=>{
+        if (answer === correctAnswer){
+            this.setState({
+                score:this.state.score + 1
+            });
+        }
+        this.setState({
+            response: this.state.response < 5 ? this.state.response + 1 : 5
+        });
+    };
+    playAgain=()=>{
+        this.getQuestions();
+        this.setState({
+            score: 0,
+            response: 0,
+        })
+    }
+    componentDidMount() {
+        this.getQuestions();
+    }
+    render(){
+        return(
+            <div className="container">
+                <div className="title">QuizApp</div>
+                {this.state.questionBank.length >0 &&
+                this.state.response < 5 &&
+                this.state.questionBank.map(
+                    ({question, answers,correct,questionId})=> (
+                        <QuestionBox 
+                        question={question} 
+                        options={answers} 
+                        key={questionId}
+                        selected={answer => this.computeAnswer(answer,correct)}
+                        />
+                    )
+                )}
+                {this.state.response === 5 ? (<Result score={this.state.score} playAgain={this.playAgain}/>): null}
+            </div>
+            )
+    }
+}
+ReactDOM.render(<QuizApp/>,document.getElementById("root"));
